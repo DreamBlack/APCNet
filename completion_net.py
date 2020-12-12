8b548eb4f3aa84a05c195d5339eb79d47f24092c
dreamblack <11041221552@qq.com>
2020-12-12 22:47:53 +0800

    fix image feature fusion bug

--- completion_net.py
+++ completion_net.py
@@ -4,6 +4,25 @@ from image_encoder import image_encoder
 from pointcloud_encoder import pointcloud_encoder
 from decoder import decoder
 from PointNetPlusEncoder import PointNetPlusEncoder
+class testNet(nn.Module):
+    def __init__(self):
+        super(testNet, self).__init__()
+        self.l1=nn.Linear(3 * 2, 2)
+        self.l2=nn.BatchNorm1d(2)
+        self.l3=nn.ReLU(True)
+
+        self.lp=nn.Linear(2, 2)
+        self.l6=nn.Sigmoid()
+
+    def forward(self, pc):
+        pc=self.l1(pc)
+        pc = self.l2(pc)
+        pc = self.l3(pc)
+        pc = self.lp(pc)
+        pc = self.l6(pc)
+        print(pc)
+        return pc
+
 class myNet(nn.Module):
     def __init__(self,c,h,w, mlp_dims, fc_dims,grid_dims, Folding1_dims,
                  Folding2_dims, Weight1_dims, Weight3_dims,dropout=0,folding=1,dropout_feature=0,attention=1,pointnetplus=0):
@@ -31,10 +50,17 @@ class myNet(nn.Module):
 
         self.pointcloud_encoder=pointcloud_encoder(mlp_dims, fc_dims)
         self.pointnetplus_encoder=PointNetPlusEncoder()
-        self.get_sig_weight=nn.Sequential(
-            nn.Linear(512 * 2, 2),
-            nn.BatchNorm1d(2),
+        # self.get_sig_weight=nn.Sequential(
+        #     nn.Linear(512 * 2, 2),
+        #     nn.BatchNorm1d(2),
+        #     nn.ReLU(True),
+        #     nn.Sigmoid()
+        # )
+        self.get_sig_weight = nn.Sequential(
+            nn.Linear(512 * 2, 256),
+            nn.BatchNorm1d(256),
             nn.ReLU(True),
+            nn.Linear(256, 2),
             nn.Sigmoid()
         )
 
@@ -89,19 +115,24 @@ class myNet(nn.Module):
 
 if __name__ == '__main__':
     b, n = 64, 1024
-    pc = torch.ones((b, n, 3))
-    image=torch.ones((b,3,128,128))
-    MLP_dims = (3, 64, 64, 64, 128, 1024)
-    FC_dims = (1024, 512, 512)
-    grid_dims = (16, 16)  # defaul 45
-    Folding1_dims = (514, 512, 512, 3)  # for foldingnet
-    Folding2_dims = (515, 512, 512, 3)  # for foldingnet
-    Weight1_dims = (16 * 16 + 512, 512, 512, 128)  # for weight matrix estimation 45x45+512 = 2537
-    Weight3_dims = (512 + 128, 1024, 1024, 256)
-    knn = 48
-    sigma = 0.008
-    model = myNet(3,128,128,MLP_dims, FC_dims,grid_dims,Folding1_dims,Folding2_dims,Weight1_dims,Weight3_dims,folding=0,attention=0,pointnetplus=1)
-    print("input_size")
-    print(pc.shape,image.shape)
-    out = model(pc,image)
-    print(out.shape)
+    x=torch.rand((4,6))
+    print(x)
+    net=testNet()
+    out=net(x)
+    print(out)
+    # pc = torch.ones((b, n, 3))
+    # image=torch.ones((b,3,128,128))
+    # MLP_dims = (3, 64, 64, 64, 128, 1024)
+    # FC_dims = (1024, 512, 512)
+    # grid_dims = (16, 16)  # defaul 45
+    # Folding1_dims = (514, 512, 512, 3)  # for foldingnet
+    # Folding2_dims = (515, 512, 512, 3)  # for foldingnet
+    # Weight1_dims = (16 * 16 + 512, 512, 512, 128)  # for weight matrix estimation 45x45+512 = 2537
+    # Weight3_dims = (512 + 128, 1024, 1024, 256)
+    # knn = 48
+    # sigma = 0.008
+    # model = myNet(3,128,128,MLP_dims, FC_dims,grid_dims,Folding1_dims,Folding2_dims,Weight1_dims,Weight3_dims,folding=0,attention=0,pointnetplus=1)
+    # print("input_size")
+    # print(pc.shape,image.shape)
+    # out = model(pc,image)
+    # print(out.shape)
