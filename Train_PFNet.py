73b0d542e3a27930ce3b2e6dacac5055be832283
dreamblack <11041221552@qq.com>
2020-12-13 10:14:33 +0800

    Train_pf_net

--- Train_PFNet.py
+++ Train_PFNet.py
@@ -12,20 +12,21 @@ import utils
 from utils import PointLoss
 from utils import distance_squre
 import data_utils as d_utils
-import ModelNet40Loader
-import shapenet_part_loader
 from model_PFNet import _netlocalD,_netG
-
+from tensorboardX import SummaryWriter
+from MyDataset_former import MyDataset
 
 
 parser = argparse.ArgumentParser()
+expdir='/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/exp/four/pf-net/Lamp'
 parser.add_argument('--dataroot',  default='dataset/train', help='path to dataset')
-parser.add_argument('--workers', type=int,default=2, help='number of data loading workers')
-parser.add_argument('--batchSize', type=int, default=24, help='input batch size')
-parser.add_argument('--pnum', type=int, default=2048, help='the point number of a sample')
-parser.add_argument('--crop_point_num',type=int,default=512,help='0 means do not use else use with this weight')
+parser.add_argument('--workers', type=int,default=16, help='number of data loading workers')
+parser.add_argument('--batchSize', type=int, default=16, help='input batch size')
+parser.add_argument('--pnum', type=int, default=1024, help='the point number of a sample')
+parser.add_argument('--crop_point_num',type=int,default=256,help='0 means do not use else use with this weight')
 parser.add_argument('--nc', type=int, default=3)
 parser.add_argument('--niter', type=int, default=201, help='number of epochs to train for')
+parser.add_argument('--class_choice', default = 'Lamp', help = 'random|center|random_center')
 parser.add_argument('--weight_decay', type=float, default=0.001)
 parser.add_argument('--learning_rate', default=0.0002, type=float, help='learning rate in training')
 parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.9')
@@ -37,7 +38,7 @@ parser.add_argument('--netD', default='', help="path to netD (to continue traini
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('--drop',type=float,default=0.2)
 parser.add_argument('--num_scales',type=int,default=3,help='number of scales')
-parser.add_argument('--point_scales_list',type=list,default=[2048,1024,512],help='number of points in each scales')
+parser.add_argument('--point_scales_list',type=list,default=[1024,512,256],help='number of points in each scales')
 parser.add_argument('--each_scales_size',type=int,default=1,help='each scales size')
 parser.add_argument('--wtl2',type=float,default=0.95,help='0 means do not use else use with this weight')
 parser.add_argument('--cropmethod', default = 'random_center', help = 'random|center|random_center')
@@ -96,15 +97,9 @@ transforms = transforms.Compose(
         d_utils.PointcloudToTensor(),
     ]
 )
-dset = shapenet_part_loader.PartDataset( root='./dataset/shapenetcore_partanno_segmentation_benchmark_v0/',classification=True, class_choice=None, npoints=opt.pnum, split='train')
+dset = MyDataset(classification=True,class_choice=opt.class_choice, split='train',four_data=1)
 assert dset
-dataloader = torch.utils.data.DataLoader(dset, batch_size=opt.batchSize,
-                                         shuffle=True,num_workers = int(opt.workers))
-
-
-test_dset = shapenet_part_loader.PartDataset( root='./dataset/shapenetcore_partanno_segmentation_benchmark_v0/',classification=True, class_choice=None, npoints=opt.pnum, split='test')
-test_dataloader = torch.utils.data.DataLoader(test_dset, batch_size=opt.batchSize,
-                                         shuffle=True,num_workers = int(opt.workers))
+dataloader = torch.utils.data.DataLoader(dset, batch_size=opt.batchSize,shuffle=True,num_workers = opt.workers)
 
 #dset = ModelNet40Loader.ModelNet40Cls(opt.pnum, train=True, transforms=transforms, download = False)
 #assert dset
@@ -154,50 +149,34 @@ if opt.D_choose == 1:
             alpha2 = 0.2
         
         for i, data in enumerate(dataloader, 0):
-            
-            real_point, target = data
-            
-    
-            batch_size = real_point.size()[0]
-            real_center = torch.FloatTensor(batch_size, 1, opt.crop_point_num, 3)       
-            input_cropped1 = torch.FloatTensor(batch_size, opt.pnum, 3)
-            input_cropped1 = input_cropped1.data.copy_(real_point)
-            real_point = torch.unsqueeze(real_point, 1)
-            input_cropped1 = torch.unsqueeze(input_cropped1,1)
-            p_origin = [0,0,0]
-            if opt.cropmethod == 'random_center':
-                #Set viewpoints
-                choice = [torch.Tensor([1,0,0]),torch.Tensor([0,0,1]),torch.Tensor([1,0,1]),torch.Tensor([-1,0,0]),torch.Tensor([-1,1,0])]
-                for m in range(batch_size):
-                    index = random.sample(choice,1)#Random choose one of the viewpoint
-                    distance_list = []
-                    p_center = index[0]
-                    for n in range(opt.pnum):
-                        distance_list.append(distance_squre(real_point[m,0,n],p_center))
-                    distance_order = sorted(enumerate(distance_list), key  = lambda x:x[1])
-                    
-                    for sp in range(opt.crop_point_num):
-                        input_cropped1.data[m,0,distance_order[sp][0]] = torch.FloatTensor([0,0,0])
-                        real_center.data[m,0,sp] = real_point[m,0,distance_order[sp][0]]
-            label.resize_([batch_size,1]).fill_(real_label)
-            real_point = real_point.to(device)
-            real_center = real_center.to(device)
-            input_cropped1 = input_cropped1.to(device)
+
+            incomplete, gt, image, filename = data
+
+            batch_size = incomplete.size()[0]
+
+            incomplete = incomplete.to(device)
+
+            gt = gt.to(device)
+            image = image.to(device)
+
+            incomplete = Variable(incomplete, requires_grad=True).cuda()
+            label.resize_([batch_size, 1]).fill_(real_label)
             label = label.to(device)
+
             ############################
             # (1) data prepare
-            ###########################      
-            real_center = Variable(real_center,requires_grad=True)
-            real_center = torch.squeeze(real_center,1)
-            real_center_key1_idx = utils.farthest_point_sample(real_center,64,RAN = False)
-            real_center_key1 = utils.index_points(real_center,real_center_key1_idx)
-            real_center_key1 =Variable(real_center_key1,requires_grad=True)
+            ###########################
+            real_center = Variable(gt, requires_grad=True)
+            real_center = torch.squeeze(real_center, 1)
+            real_center_key1_idx = utils.farthest_point_sample(real_center, 64, RAN=False)
+            real_center_key1 = utils.index_points(real_center, real_center_key1_idx)
+            real_center_key1 = Variable(real_center_key1, requires_grad=True)
 
-            real_center_key2_idx = utils.farthest_point_sample(real_center,128,RAN = True)
-            real_center_key2 = utils.index_points(real_center,real_center_key2_idx)
-            real_center_key2 =Variable(real_center_key2,requires_grad=True)
+            real_center_key2_idx = utils.farthest_point_sample(real_center, 128, RAN=True)
+            real_center_key2 = utils.index_points(real_center, real_center_key2_idx)
+            real_center_key2 = Variable(real_center_key2, requires_grad=True)
 
-            input_cropped1 = torch.squeeze(input_cropped1,1)
+            input_cropped1 = torch.squeeze(incomplete,1)
             input_cropped2_idx = utils.farthest_point_sample(input_cropped1,opt.point_scales_list[1],RAN = True)
             input_cropped2     = utils.index_points(input_cropped1,input_cropped2_idx)
             input_cropped3_idx = utils.farthest_point_sample(input_cropped1,opt.point_scales_list[2],RAN = False)
@@ -243,74 +222,37 @@ if opt.D_choose == 1:
             errG = (1-opt.wtl2) * errG_D + opt.wtl2 * errG_l2
             errG.backward()
             optimizerG.step()
-            print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f / %.4f / %.4f/ %.4f'
-                  % (epoch, opt.niter, i, len(dataloader), 
-                     errD.data, errG_D.data,errG_l2,errG,CD_LOSS))
-            f=open('loss_PFNet1.txt', 'a')
-            f.write('\n'+'[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f / %.4f / %.4f /%.4f'
-                  % (epoch, opt.niter, i, len(dataloader), 
-                     errD.data, errG_D.data,errG_l2,errG,CD_LOSS))
-            
-            
-            if i % 10 ==0:
-                print('After, ',i,'-th batch')
-                f.write('\n'+'After, '+str(i)+'-th batch')
-                for i, data in enumerate(test_dataloader, 0):
-                    real_point, target = data
-                    
-            
-                    batch_size = real_point.size()[0]
-                    real_center = torch.FloatTensor(batch_size, 1, opt.crop_point_num, 3)
-                    input_cropped1 = torch.FloatTensor(batch_size, opt.pnum, 3)
-                    input_cropped1 = input_cropped1.data.copy_(real_point)
-                    real_point = torch.unsqueeze(real_point, 1)
-                    input_cropped1 = torch.unsqueeze(input_cropped1,1)
-                    
-                    p_origin = [0,0,0]
-                    
-                    if opt.cropmethod == 'random_center':
-                        choice = [torch.Tensor([1,0,0]),torch.Tensor([0,0,1]),torch.Tensor([1,0,1]),torch.Tensor([-1,0,0]),torch.Tensor([-1,1,0])]
-                        
-                        for m in range(batch_size):
-                            index = random.sample(choice,1)
-                            distance_list = []
-                            p_center = index[0]
-                            for n in range(opt.pnum):
-                                distance_list.append(distance_squre(real_point[m,0,n],p_center))
-                            distance_order = sorted(enumerate(distance_list), key  = lambda x:x[1])                         
-                            for sp in range(opt.crop_point_num):
-                                input_cropped1.data[m,0,distance_order[sp][0]] = torch.FloatTensor([0,0,0])
-                                real_center.data[m,0,sp] = real_point[m,0,distance_order[sp][0]]  
-                    real_center = real_center.to(device)
-                    real_center = torch.squeeze(real_center,1)
-                    input_cropped1 = input_cropped1.to(device) 
-                    input_cropped1 = torch.squeeze(input_cropped1,1)
-                    input_cropped2_idx = utils.farthest_point_sample(input_cropped1,opt.point_scales_list[1],RAN = True)
-                    input_cropped2     = utils.index_points(input_cropped1,input_cropped2_idx)
-                    input_cropped3_idx = utils.farthest_point_sample(input_cropped1,opt.point_scales_list[2],RAN = False)
-                    input_cropped3     = utils.index_points(input_cropped1,input_cropped3_idx)
-                    input_cropped1 = Variable(input_cropped1,requires_grad=False)
-                    input_cropped2 = Variable(input_cropped2,requires_grad=False)
-                    input_cropped3 = Variable(input_cropped3,requires_grad=False)
-                    input_cropped2 = input_cropped2.to(device)
-                    input_cropped3 = input_cropped3.to(device)      
-                    input_cropped  = [input_cropped1,input_cropped2,input_cropped3]
-                    point_netG.eval()
-                    fake_center1,fake_center2,fake  =point_netG(input_cropped)
-                    CD_loss = criterion_PointLoss(torch.squeeze(fake,1),torch.squeeze(real_center,1))
-                    print('test result:',CD_loss)
-                    f.write('\n'+'test result:  %.4f'%(CD_loss))
-                    break
-            f.close()
+            writer = SummaryWriter(log_dir=os.path.join(expdir,'tensorboard'))
+            writer.add_scalar('cd_missing', CD_LOSS, num_batch * epoch + i)
+            writer.add_scalar('D_Loss', errD.data, num_batch * epoch + i)
+            writer.add_scalar('GD_Loss', errG_D.data, num_batch * epoch + i)
+            writer.add_scalar('GwithD_sum_loss', errG, num_batch * epoch + i)
+            writer.add_scalar('GwithD_l2', errG_l2, num_batch * epoch + i)
+            complete_pc = torch.cat([torch.squeeze(fake,1), incomplete], dim=1)
+            complete_gt = torch.cat([gt, incomplete], dim=1)
+            CD_LOSS_ALL = criterion_PointLoss(torch.squeeze(complete_pc, 1).cuda(),
+                                              torch.squeeze(complete_gt, 1).cuda())
+            CD_LOSS_ALL = CD_LOSS_ALL.data.cpu()
+            writer.add_scalar('cd_all', CD_LOSS_ALL, num_batch * epoch + i)
+            writer.add_scalar('lr', schedulerG.get_lr()[0], num_batch * epoch + i)
+            writer.close()
+            print(
+                '[%d/%d][%d/%d] [missing_cd/all_cd/d_loss/GD_loss/GwithD_l2/GwithD_sum_loss]:  %.4f / %.4f / %.4f / %.4f / %.4f / %.4f '
+                % (epoch, opt.niter, i, len(dataloader),
+                   CD_LOSS, CD_LOSS_ALL, errD.data, errG_D.data, errG_l2, errG))
+
         schedulerD.step()
         schedulerG.step()
-        if epoch% 10 == 0:   
-            torch.save({'epoch':epoch+1,
-                        'state_dict':point_netG.state_dict()},
-                        'Trained_Model/point_netG'+str(epoch)+'.pth' )
-            torch.save({'epoch':epoch+1,
-                        'state_dict':point_netD.state_dict()},
-                        'Trained_Model/point_netD'+str(epoch)+'.pth' )  
+        if epoch % 10 == 0:
+            torch.save({'epoch': epoch + 1,
+                        'state_dict': point_netG.state_dict()},
+                       expdir + '/checkpoint/point_netG' + str(epoch) + '.pth')
+            torch.save({'epoch': epoch + 1,
+                        'state_dict': point_netD.state_dict()},
+                       expdir + '/checkpoint/point_netD' + str(epoch) + '.pth')
+            
+            
+
 
 #
 #############################
@@ -399,7 +341,7 @@ else:
             print('[%d/%d][%d/%d] Loss_G: %.4f / %.4f '
                   % (epoch, opt.niter, i, len(dataloader), 
                       errG_l2,CD_LOSS))
-            f=open('loss_PFNet1.txt', 'a')
+            f=open('loss_PFNet.txt','a')
             f.write('\n'+'[%d/%d][%d/%d] Loss_G: %.4f / %.4f '
                   % (epoch, opt.niter, i, len(dataloader), 
                       errG_l2,CD_LOSS))
