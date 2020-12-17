c050efad3e08cdeb41e50f2809ae9ac6f6db3112
dreamblack <11041221552@qq.com>
2020-12-17 20:43:39 +0800

    dd

---
+++ former_comnet.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+from image_encoder import image_encoder
+from pointcloud_encoder import pointcloud_encoder
+from decoder import decoder
+from PointNetPlusEncoder import PointNetPlusEncoder
+class testNet(nn.Module):
+    def __init__(self):
+        super(testNet, self).__init__()
+        self.l1=nn.Linear(3 * 2, 2)
+        self.l2=nn.BatchNorm1d(2)
+        self.l3=nn.ReLU()
+
+        self.lp=nn.Linear(2, 2)
+        self.l6=nn.Sigmoid()
+
+    def forward(self, pc):
+        pc=self.l1(pc)
+        pc = self.l2(pc)
+        pc = self.l3(pc)
+        pc = self.lp(pc)
+        pc = self.l6(pc)
+        print(pc)
+        return pc
+
+class myNet(nn.Module):
+    def __init__(self,c,h,w, mlp_dims, fc_dims,grid_dims, Folding1_dims,
+                 Folding2_dims, Weight1_dims, Weight3_dims,dropout=0,folding=1,dropout_feature=0,attention=1,pointnetplus=0):
+        assert (mlp_dims[-1] == fc_dims[0])
+        super(myNet, self).__init__()
+        self.folding = folding
+        self.attention=attention
+        self.dropout=dropout
+        self.pointnetplus=pointnetplus
+        self.image_encoder = image_encoder(c,h,w,self.attention)
+        if attention==1:
+            print("Use attention in image encoder")
+        else:
+            print("Do not use attention in image encoder")
+
+        if folding==1:
+            print("Use folding as decoder")
+        else:
+            print("Use fc as decoder")
+
+        if pointnetplus==1:
+            print("Use pointnetplus as pointcloud encoder")
+        else:
+            print("Use pointnet as pointcloud encoder")
+
+        self.pointcloud_encoder=pointcloud_encoder(mlp_dims, fc_dims)
+        self.pointnetplus_encoder=PointNetPlusEncoder()
+        self.get_sig_weight=nn.Sequential(
+            nn.Linear(512 * 2, 2),
+            nn.BatchNorm1d(2),
+            nn.ReLU(True),
+            nn.Sigmoid()
+        )
+        # self.get_sig_weight = nn.Sequential(
+        #     nn.Linear(512 * 2, 256),
+        #     nn.BatchNorm1d(256),
+        #     nn.ReLU(),
+        #     nn.Linear(256, 2),
+        #     nn.Sigmoid()
+        # )
+
+        self.feature_fusion=nn.Sequential(
+            nn.Linear(1024 , 1024),
+            #nn.Dropout(dropout_feature),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            #nn.Dropout(dropout_feature),
+            nn.BatchNorm1d(512),
+            nn.ReLU()
+        )
+        self.decoder=decoder(grid_dims, Folding1_dims,
+                 Folding2_dims, Weight1_dims, Weight3_dims)
+        self.fc_decoder = nn.Sequential(
+            nn.Linear(512, 1024),
+            nn.Dropout(dropout),
+            #nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 1024),
+            nn.Dropout(dropout),
+            #nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 256 * 3)
+        )
+
+    def forward(self, pc,image):
+        # B*N*3,B*H*W
+        image_feature=self.image_encoder.forward(image) # B*1*C
+        if  self.pointnetplus==0:
+            pc_feature=self.pointcloud_encoder.forward(pc) # B*1*c
+        else:
+            pc_feature = self.pointnetplus_encoder.forward(pc)  # B*1*c
+
+        cat_feature = torch.cat((image_feature, pc_feature), 1)  # b*2*(c)
+        fc_cat_feature = cat_feature.view(cat_feature.size(0), 1024)
+        sig_weight=self.get_sig_weight(fc_cat_feature)
+
+        sig_weight=sig_weight.unsqueeze(2)
+        cat_feature = torch.mul(sig_weight, cat_feature)
+        fusion_feature = cat_feature.view(cat_feature.size(0), 1024)
+        fusion_feature=self.feature_fusion(fusion_feature)
+        if self.folding==1:
+            fusion_feature = fusion_feature.unsqueeze(1)
+            f, _ = self.decoder.forward(fusion_feature)
+        else:
+            f = self.fc_decoder(fusion_feature)
+            f = f.view(-1, 256, 3)
+        return f
+
+
+if __name__ == '__main__':
+    b, n = 64, 1024
+    x=torch.rand((4,6))
+    print(x)
+    net=testNet()
+    out=net(x)
+    print(out)
+    # pc = torch.ones((b, n, 3))
+    # image=torch.ones((b,3,128,128))
+    # MLP_dims = (3, 64, 64, 64, 128, 1024)
+    # FC_dims = (1024, 512, 512)
+    # grid_dims = (16, 16)  # defaul 45
+    # Folding1_dims = (514, 512, 512, 3)  # for foldingnet
+    # Folding2_dims = (515, 512, 512, 3)  # for foldingnet
+    # Weight1_dims = (16 * 16 + 512, 512, 512, 128)  # for weight matrix estimation 45x45+512 = 2537
+    # Weight3_dims = (512 + 128, 1024, 1024, 256)
+    # knn = 48
+    # sigma = 0.008
+    # model = myNet(3,128,128,MLP_dims, FC_dims,grid_dims,Folding1_dims,Folding2_dims,Weight1_dims,Weight3_dims,folding=0,attention=0,pointnetplus=1)
+    # print("input_size")
+    # print(pc.shape,image.shape)
+    # out = model(pc,image)
+    # print(out.shape)
--- test_fig.py
+++ test_fig.py
@@ -1,4 +1,7 @@
 import sys
+
+import cv2
+
 sys.path.append('/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network')
 import argparse
 import random
@@ -9,22 +12,31 @@ from MyDataset_former import MyDataset
 from completion_net import myNet
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pf_net_three import  _netG
+import former_comnet
+import utils
+import os
 parser = argparse.ArgumentParser()  # create an argumentparser object
+parser.add_argument('--pccenter', type=int,default=1, help='number of data loading workers')
+parser.add_argument('--imageview',  default="00", help='input batch size')
+parser.add_argument('--obj_id',  default="1a56d596c77ad5936fa87a658faf1d26", help='input batch size')
 parser.add_argument('--workers', type=int,default=2, help='number of data loading workers')
 parser.add_argument('--batchSize', type=int, default=24, help='input batch size')
-parser.add_argument('--class_choice', default='Lamp', help="which class choose to train")
+parser.add_argument('--class_choice', default='Car', help="which class choose to train")
 parser.add_argument('--attention_encoder', type = int, default = 1, help='enables cuda')
 parser.add_argument('--folding_decoder', type = int, default = 1, help='enables cuda')
 parser.add_argument('--pointnetplus_encoder', type = int, default = 0, help='enables cuda')
 parser.add_argument('--cuda', type = bool, default = True, help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=2, help='number of GPUs to use')
-parser.add_argument('--netG', help="path to netG (to load as model)")
+parser.add_argument('--pathA', default="/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/exp/folding_rep_attention/02958343/checkpoint/point_netG130.pth")
+parser.add_argument('--pathB', default="/home/dream/study/codes/PCCompletion/best_three_exp/pfnet/car/checkpoint/point_netG130.pth")
 parser.add_argument('--result_path', help="path to netG (to load as model)")
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('--four_data', type = int, default = 0, help='enables cuda')
+parser.add_argument('--index', type = int, default = 1, help='enables cuda')
 opt = parser.parse_args()
 print(opt)
+
 
 # path_Nets=['/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/Checkpoint (复件)/point_netG150.pth',
 #            '/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/withDCkPath/Trained_Model/point_netG50.pth',
@@ -32,7 +44,9 @@ print(opt)
 #            '/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/withDCkPath/WITHDAR/Trained_Model/point_netG130.pth']
 #path='/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/fc_decoder_withG_exp/checkpoint/Trained_Model/point_netG100.pth'
 path='/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/exp/folding_rep_attention/02958343/checkpoint/point_netG150.pth'
-path_Nets=[opt.netG]
+path_Nets=[opt.pathA]
+netpaths=['/home/dream/study/codes/PCCompletion/best_three_exp/lamp/checkpoint/point_netG130.pth',
+'/home/dream/study/codes/PCCompletion/PFNet/PF-Net-Point-Fractal-Network/noDCkPath/Checkpoint/point_netG120.pth']
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 cudnn.benchmark = True # promote
 MLP_dimsG = (3, 64, 64, 64, 128, 1024)
@@ -57,9 +71,153 @@ print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 torch.cuda.manual_seed_all(opt.manualSeed)
-incompleteColor='gray'
+
+#inin my net
+# net = myNet(3, 128, 128, MLP_dimsG, FC_dimsG, grid_dims, Folding1_dims, Folding2_dims, Weight1_dims,
+#             Weight3_dims, folding=opt.folding_decoder, attention=opt.attention_encoder,
+#             pointnetplus=opt.pointnetplus_encoder)
+# net = torch.nn.DataParallel(net)
+# net.to(device)
+# net.load_state_dict(torch.load(opt.pathA, map_location=lambda storage, location: storage)['state_dict'],
+#                     strict=False)
+# net.eval()
+
+
+image_home='/home/dream/study/codes/densepcr/datasets/rendering'
+pc_home='/home/dream/study/codes/PCCompletion/datasets/dataFromPFNet/shapenet_part/shapenet_part/datagenerate'
+catname_id={'Car':"02958343",'Lamp':"03636649"}
+def get_mode_by_name(cat,filename,imageview='00',pccenter=0):
+    image_path=os.path.join(image_home,catname_id[cat],filename,"rendering",imageview+".png")
+    input_crop_path=os.path.join(pc_home,catname_id[cat],str(pccenter)+"incomplete"+filename+".pts")
+    input_gt_path=os.path.join(pc_home,catname_id[cat],str(pccenter)+"gt"+filename+".pts")
+    print("image path %s"%image_path)
+    print("input_crop_path %s"%input_crop_path)
+
+    print("input_gt_path %s"%input_gt_path)
+    if os.path.exists(image_path)==False or os.path.exists(image_path)==False or os.path.exists(image_path)==False:
+        print("path is no correct. Exit!")
+        exit(0)
+
+    incomplete = np.loadtxt(input_crop_path).astype(np.float32)
+
+    gt = np.loadtxt(input_gt_path).astype(np.float32)
+    image = cv2.imread(image_path)[4:-5, 4:-5, :3]
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    incomplete = torch.from_numpy(incomplete)
+    gt = torch.from_numpy(gt)
+    image = torch.from_numpy(np.transpose(image, (2, 0, 1)))
+    return incomplete,gt,image
+
+def get_result_for_pfNet(incomplete):
+    point_netG = _netG(3, 1, [1024, 512, 256], 256)
+    point_netG = torch.nn.DataParallel(point_netG)
+    point_netG.to(device)
+    point_netG.load_state_dict(torch.load(opt.pathB, map_location=lambda storage, location: storage)['state_dict'],
+                        strict=False)
+    point_netG.eval()
+
+    ############################
+    # (1) data prepare
+    ###########################
+    input_cropped1 = torch.squeeze(incomplete, 1)
+    input_cropped2_idx = utils.farthest_point_sample(input_cropped1, 512, RAN=True)
+    input_cropped2 = utils.index_points(input_cropped1, input_cropped2_idx)
+    input_cropped3_idx = utils.farthest_point_sample(input_cropped1, 256, RAN=False)
+    input_cropped3 = utils.index_points(input_cropped1, input_cropped3_idx)
+    input_cropped1 = Variable(input_cropped1, requires_grad=False)
+    input_cropped2 = Variable(input_cropped2, requires_grad=False)
+    input_cropped3 = Variable(input_cropped3, requires_grad=False)
+    input_cropped2 = input_cropped2.to(device)
+    input_cropped3 = input_cropped3.to(device)
+    input_cropped = [input_cropped1, input_cropped2, input_cropped3]
+    fake_center1, fake_center2, fake = point_netG(input_cropped)
+    return fake.cuda().data.cpu().squeeze(0).numpy()
+
+def get_result_for_myNet(incomplete,image,netpath):
+    net = myNet(3, 128, 128, MLP_dimsG, FC_dimsG, grid_dims, Folding1_dims, Folding2_dims, Weight1_dims,
+                Weight3_dims, folding=opt.folding_decoder, attention=opt.attention_encoder,
+                pointnetplus=opt.pointnetplus_encoder)
+    net = torch.nn.DataParallel(net)
+    net.to(device)
+    net.load_state_dict(torch.load(netpath, map_location=lambda storage, location: storage)['state_dict'],
+                        strict=False)
+    net.eval()
+
+    fake = net(incomplete, image)
+    return fake.cuda().data.cpu().squeeze(0).numpy()
+def get_result_for_myNet2(incomplete,image,netpath):
+    net2 = former_comnet.myNet(3, 128, 128, MLP_dimsG, FC_dimsG, grid_dims, Folding1_dims, Folding2_dims, Weight1_dims,
+                Weight3_dims, folding=opt.folding_decoder, attention=opt.attention_encoder,
+                pointnetplus=opt.pointnetplus_encoder)
+    net2 = torch.nn.DataParallel(net2)
+    net2.to(device)
+    net2.load_state_dict(torch.load(netpath, map_location=lambda storage, location: storage)['state_dict'],
+                        strict=False)
+    net2.eval()
+
+    fake = net2(incomplete, image)
+    return fake.cuda().data.cpu().squeeze(0).numpy()
+def compare_my_and_pf(cat,obj_id,imageview,pccenter,mynmet_path):
+
+    incomplete,gt,image=get_mode_by_name(cat,obj_id,imageview,pccenter)
+
+    my_image = np.transpose(image, (1, 2, 0))
+    incomplete = torch.unsqueeze(incomplete, 0)
+    image = torch.unsqueeze(image, 0)
+
+    incomplete = incomplete.to(device)
+    image = image.to(device)
+
+    incomplete = Variable(incomplete, requires_grad=False)
+    image = Variable(image.float(), requires_grad=False)
+
+
+    pf_fake=get_result_for_pfNet(incomplete)
+    my_fakes=[]
+    my_fakes.append(get_result_for_myNet2(incomplete,image,opt.pathA))#mynmet_path[0]
+    #my_fakes.append(get_result_for_myNet2(incomplete, image, mynmet_path[1]))
+
+    incomplete=incomplete.cuda().data.squeeze(0).cpu().numpy()
+    fig = plt.figure(figsize=(24, 12), facecolor='w')  #
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.9)
+
+    ax = fig.add_subplot(141)
+    fig.set_facecolor('none')
+    ax.imshow(my_image)
+    plt.title("input image")
+    plt.axis("off")
+
+    ax0 = fig.add_subplot(142, projection='3d')
+    ax0.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
+    ax0.scatter(gt[:, 0], gt[:, 1], gt[:, 2], color=recColor, s=pointSize)
+    ax0.set_axis_off()
+    plt.title("GT")
+
+    ax1 = fig.add_subplot(143, projection='3d')
+    ax1.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
+    ax1.scatter(pf_fake[:, 0], pf_fake[:, 1], pf_fake[:, 2], color=recColor, s=pointSize)
+    ax1.set_axis_off()
+    plt.title("PF-Net")
+
+    ax2 = fig.add_subplot(144, projection='3d')
+    ax2.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
+    ax2.scatter(my_fakes[0][:, 0], my_fakes[0][:, 1], my_fakes[0][:, 2], color=recColor, s=pointSize)
+    ax2.set_axis_off()
+    plt.title("MyGood")
+
+    # ax3 = fig.add_subplot(155, projection='3d')
+    # ax3.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
+    # ax3.scatter(my_fakes[1][:, 0], my_fakes[1][:, 1], my_fakes[1][:, 2], color=recColor, s=pointSize)
+    # ax3.set_axis_off()
+    # plt.title("My2")
+
+    plt.show()
+
+
+incompleteColor='lightgray'
 recColor='royalblue'
-pointSize=60
+pointSize=55
 def draw_point_cloud(incomplete,rec_missing, elev=0,azim=0,output_filename=None):
     """ points is a Nx3 numpy array """
     fig = plt.figure(figsize=(24, 12), facecolor='w')  #
@@ -76,22 +234,32 @@ def draw_point_cloud(incomplete,rec_missing, elev=0,azim=0,output_filename=None)
     plt.show()
 
 #画出3d点云,image+gt+result
-def pyplot_draw_point_cloud(image,incomplete,rec_missing, elev=0,azim=0,output_filename=None):
+def pyplot_draw_point_cloud(image,incomplete,rec_missing,modelnum=2, elev=0,azim=0,output_filename=None):
     """ points is a Nx3 numpy array """
     fig = plt.figure(figsize=(24, 12), facecolor='w')  #
     plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.9)
 
-    ax = fig.add_subplot(131)
+    ax = fig.add_subplot(141)
+    #image = np.transpose(image, (1,0,2))
+    fig.set_facecolor('none')
+    #image=np.fliplr(image.reshape(-1,3)).reshape(image.shape)
     ax.imshow(image)
     plt.title("input image")
+    plt.axis("off")
 
-    ax0 = fig.add_subplot(132, projection='3d')
+    ax0 = fig.add_subplot(142, projection='3d')
     ax0.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
     ax0.scatter(missings[0][:, 0], missings[0][:, 1], missings[0][:, 2], color=recColor, s=pointSize)
     ax0.set_axis_off()
     plt.title("GT")
 
-    ax1 = fig.add_subplot(133, projection='3d')
+    ax1 = fig.add_subplot(143, projection='3d')
+    ax1.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
+    ax1.scatter(missings[1][:, 0], missings[1][:, 1], missings[1][:, 2], color=recColor, s=pointSize)
+    ax1.set_axis_off()
+    plt.title("Vanilla")
+
+    ax1 = fig.add_subplot(144, projection='3d')
     ax1.scatter(incomplete[:, 0], incomplete[:, 1], incomplete[:, 2], color=incompleteColor, s=pointSize)
     ax1.scatter(missings[1][:, 0], missings[1][:, 1], missings[1][:, 2], color=recColor, s=pointSize)
     ax1.set_axis_off()
@@ -171,40 +339,26 @@ if __name__ == '__main__':
 
     no=True
     if no:
-        # if opt.class_choice=='Car':
-        #     MLP_dimsG = (3, 64, 64, 64, 128, 512)
-        #     FC_dimsG = (512, 512, 512)
         missings = []
-        incomplete, gt, image, filename = test_dset.__getitem__(opt.index)
+        #incomplete, gt, image, filename = test_dset.__getitem__(opt.index)
+        gt=torch.rand((4,6))
         missings.append(gt)
-        my_image = np.transpose(image, (1, 2, 0))
 
-        incomplete = torch.unsqueeze(incomplete, 0)
-        image = torch.unsqueeze(image, 0)
 
-        incomplete = incomplete.to(device)
-        image = image.to(device)
+        compare_my_and_pf(opt.class_choice,opt.obj_id,opt.imageview,opt.pccenter,netpaths) # (cat,obj_id,imageview,pccenter):
 
-        incomplete = Variable(incomplete, requires_grad=False)
-        image = Variable(image.float(), requires_grad=False)
-
-        for i in range(1):
-            net = myNet(3, 128, 128, MLP_dimsG, FC_dimsG, grid_dims, Folding1_dims, Folding2_dims, Weight1_dims,
-                        Weight3_dims, folding=opt.folding_decoder, attention=opt.attention_encoder,pointnetplus=opt.pointnetplus_encoder)
-
-            net = torch.nn.DataParallel(net)
-            net.to(device)
-            net.load_state_dict(torch.load(path_Nets[i], map_location=lambda storage, location: storage)['state_dict'],strict=False)
-            net.eval()
-            fake = net(incomplete, image)
-            missings.append(fake.cuda().data.cpu().squeeze(0).numpy())
-
-        print(len(missings))
-        print(incomplete.cuda().data.squeeze(0).shape)
-
+        # for i in range(1):
+        #
+        #     fake = net(incomplete, image)
+        #     missings.append(fake.cuda().data.cpu().squeeze(0).numpy())
+        #
+        # print(len(missings))
+        # print(incomplete.cuda().data.squeeze(0).shape)
         # 显示单个结果
         # pyplot_draw_point_cloud(my_image,incomplete.cuda().data.squeeze(0).cpu().numpy(),missings)
-        pyplot_draw_point_cloud(my_image, incomplete.cuda().data.squeeze(0).cpu().numpy(), missings)
+
+
+        #pyplot_draw_point_cloud(my_image, incomplete.cuda().data.squeeze(0).cpu().numpy(), missings)
 
         # 构建incomplete和missings数组
         # pyplot_for_one_object(my_image,incomplete.cuda().data.squeeze(0).cpu().numpy(),missings)
