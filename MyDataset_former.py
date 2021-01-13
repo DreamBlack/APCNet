15aa7736078199e7b9bc577b2b96cedbfe659f36
dreamblack <11041221552@qq.com>
2021-01-13 13:37:37 +0800

    add compare method folding

--- MyDataset_former.py
+++ MyDataset_former.py
@@ -14,9 +14,11 @@ help_path='/home/dream/study/codes/PCCompletion/datasets/dataFromPFNet/shapenet_
 dsimage_path='/home/dream/study/codes/densepcr/datasets/ShapeNet/ShapeNetRendering/'
 dataset_pathT='/home/dream/study/codes/PCCompletion/datasets/dataFromPFNet/shapenet_part/shapenet_part/datagenerate/'
 dataset_pathF='/home/dream/study/codes/PCCompletion/datasets/dataFromPFNet/shapenet_part/shapenet_part/datagenerateForFour/'
+dataset_pathCmp='/home/dream/study/codes/PCCompletion/datasets/dataFromPFNet/shapenet_part/shapenet_part/completepc/'
+
 plt.figure()
 class MyDataset(data.Dataset):
-    def __init__(self, root=dataset_pathT, three=1, classification=False, class_choice=None, split='train',four_data=0):
+    def __init__(self, root=dataset_pathT, three=1, classification=False, class_choice=None, split='train',four_data=0,cmpfolding=0):
         if  three==0 or four_data==1:
             root=dataset_pathF
         self.root = root # pointcloud path
@@ -50,6 +52,7 @@ class MyDataset(data.Dataset):
 
             #  self.cat[item] is id
             dir_point = os.path.join(self.root, self.cat[item])
+            dir_cmp_pt = os.path.join(dataset_pathCmp, self.cat[item]) #
             dir_image = os.path.join(self.image_path, self.cat[item])
             dir_help=os.path.join(self.help_path, self.cat[item],'points')
 
@@ -85,6 +88,8 @@ class MyDataset(data.Dataset):
 
                 good_center=[]
                 image_view = ['00', '09', '18']
+                image_view = ['00']
+
 
                 if four_data==1 or three==0:
                     for view in image_view:
@@ -93,12 +98,15 @@ class MyDataset(data.Dataset):
                              os.path.join(dir_point, 'gt' + token + '.pts'),
                              os.path.join(dir_image, token, 'rendering', view + '.png'),
                              self.cat[item],
-                             token))
+                             token,
+                             os.path.join(dir_cmp_pt, 'cmpgt' + token + '.pts')))
                 else:
                     if class_choice=='Lamp':
                         good_center=[0,1,2,6,7]
                     else:
                         good_center = [0, 1, 2, 3, 4]
+                    image_view = ['00', '09', '18']
+                    image_view = ['00']
                     for center in good_center:
                         for view in image_view:
                             self.meta[item].append(
@@ -106,21 +114,14 @@ class MyDataset(data.Dataset):
                                  os.path.join(dir_point, str(center) + 'gt' + token + '.pts'),
                                  os.path.join(dir_image, token, 'rendering', view + '.png'),
                                  self.cat[item],
-                                 token))
-                    # for center in range(8):
-                    #     for view in image_view:
-                    #         self.meta[item].append(
-                    #             (os.path.join(dir_point, str(center) + 'incomplete' + token + '.pts'),
-                    #              os.path.join(dir_point, str(center) + 'gt' + token + '.pts'),
-                    #              os.path.join(dir_image, token, 'rendering', view + '.png'),
-                    #              self.cat[item],
-                    #              token))
+                                 token,
+                                 os.path.join(dir_cmp_pt, 'cmpgt' + token + '.pts')))
 
 
         self.datapath = []  #  所有path的总和
         for item in self.cat:
             for fn in self.meta[item]:
-                self.datapath.append((item, fn[0], fn[1], fn[2], fn[3],fn[4]))
+                self.datapath.append((item, fn[0], fn[1], fn[2], fn[3],fn[4],fn[5]))
 
         self.classes = dict(zip(sorted(self.cat), range(len(self.cat))))  # zip(a,b)将两个元组打包为列表909033,0的形式，dict又将其变为字典
 
@@ -138,7 +139,7 @@ class MyDataset(data.Dataset):
     def __getitem__(self, index):
         if index in self.cache:
             #            in, gt,image= self.cache[index]
-            incomplete, gt, image, foldername, filename= self.cache[index]
+            incomplete, gt, image, foldername, filename,cmpgt= self.cache[index]
         else:
             fn = self.datapath[index]  # filename
             cls = self.classes[self.datapath[index][0]]
@@ -148,19 +149,13 @@ class MyDataset(data.Dataset):
             gt = np.loadtxt(fn[2]).astype(np.float32)
             image = cv2.imread(fn[3])[4:-5, 4:-5, :3]
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+            cmpgt = np.loadtxt(fn[6]).astype(np.float32)
 
             foldername = fn[4]
             filename = fn[5]
 
             if len(self.cache) < self.cache_size:
-                self.cache[index] = (incomplete, gt, image, foldername, filename)
-
-        # print(point_set.shape, seg.shape)
-        # choice = np.random.choice(len(seg), self.npoints, replace=True)  # random.choic(a，size，true),返回a中大小为size的随机一项，有放回
-        # resample
-        # point_set = point_set[choice, :]
-        # seg = seg[choice]
+                self.cache[index] = (incomplete, gt, image, foldername, filename,cmpgt)
 
         # To Pytorch
         incomplete = torch.from_numpy(incomplete)
@@ -170,14 +165,14 @@ class MyDataset(data.Dataset):
             view_id=os.path.basename(fn[3])[0:2]
             center_id=os.path.basename(fn[2])[0]
             if self.classification:
-                return incomplete, gt, image, filename,view_id,center_id
+                return incomplete, gt, image, filename,cmpgt,view_id,center_id
             else:
-                return incomplete, gt, image, filename,view_id,center_id
+                return incomplete, gt, image, filename,cmpgt,view_id,center_id
         else:
             if self.classification:
-                return incomplete, gt, image, filename
+                return incomplete, gt, image, filename,cmpgt
             else:
-                return incomplete, gt, image, filename
+                return incomplete, gt, image, filename,cmpgt
 
     def __len__(self):
         return len(self.datapath)
@@ -198,7 +193,8 @@ if __name__ == '__main__':
     dataloader = torch.utils.data.DataLoader(dset, batch_size=16, shuffle=True, num_workers=16)
     for epoch in range(0, 10):
         for i, data in enumerate(dataloader, 0):
-            incomplete, gt, image, filename = data
+            incomplete, gt, image, filename,cmpgt = data
+            np.savetxt('cmppc' + '.pts', cmpgt[0], fmt = "%f %f %f")
             print("filename %d: %s"%(epoch,filename))
             break
     #    d = PartDataset( root='./dataset/shapenetcore_partanno_segmentation_benchmark_v0/',classification=False, class_choice=None, npoints=4096, split='test')
